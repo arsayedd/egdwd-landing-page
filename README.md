e68612517de339a0f796dd2d93f4b6a481fb169e
Ahmed Raafat Sayed <113071985+arsayedd@users.noreply.github.com>
2022-12-20 20:57:42 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# Landing Page Project
+
+The project is about creating a responsive page, with a responsive nav-bar
+ 
+### List of files & folders included in the project
+
+ - *Index.html*
+ - js 
+	 - *app.js*
+ - css
+	 - *styles.css*
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+#### I worked on this project all on my own.
+
+#### Websites used to help me finish this project
+
+ 1. https://www.w3schools.com/
+ 2. https://developer.mozilla.org/en-US/
+ 3. https://www.javascript.com/
+ 4. https://stackoverflow.com/
+ 5. https://elzero.org/
+ 6. https://www.youtube.com/
+
---
+++ js/app.js
@@ -0,0 +1,105 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const navList=document.querySelector('#Navbar_list');
+const dataSections=document.querySelectorAll('section');
+const sectoinsCount=dataSections.length;
+let activeSectionIndex=0;
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+const isInViewport = (section)=>{
+    const rect = section.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight) &&
+        rect.right <= (window.innerWidth|| document.documentElement.clientWidth|| document.body.clientWidth)
+    );
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+const buildNav = ()=>{
+    const hiddenList = document.createDocumentFragment();
+    for(const section of dataSections){
+        const listItem = document.createElement('li');
+        const anchorItem = document.createElement('a');
+        anchorItem.addEventListener('click',(event)=>{
+            event.preventDefault();
+            window.scrollTo({
+                top: section.offsetTop,
+                left: 0,
+                behavior: 'smooth'
+              });
+        });
+        anchorItem.classList.add('menu_link');
+        anchorItem.setAttribute('href',`#${section.id}`);
+        anchorItem.innerText=section.getAttribute('data-nav');
+        listItem.appendChild(anchorItem);
+        hiddenList.appendChild(listItem);
+    }
+    navList.appendChild(hiddenList);
+
+}
+
+// Add class 'active' to section when near top of viewport
+const getActiveSection = ()=>{
+    let activeSectionIdx=activeSectionIndex;
+    for(let sectionIndex=0; sectionIndex<sectoinsCount; sectionIndex++){
+        if(isInViewport(dataSections[sectionIndex]))
+            activeSectionIdx=sectionIndex;
+    }
+    return activeSectionIdx;
+}
+
+const activateSection = ()=>{
+    if(sectoinsCount==0)return;
+    const lastActiveSection = dataSections[activeSectionIndex];
+    activeSectionIndex=getActiveSection();
+    activeSection=dataSections[activeSectionIndex];
+    lastActiveSection.classList.remove('active__section');
+    document.querySelector(`a[href="#${lastActiveSection.id}"]`).classList.remove('active__link');
+    activeSection.classList.add('active__section');
+    document.querySelector(`a[href="#${activeSection.id}"]`).classList.add('active__link');
+}
+// Scroll to anchor ID using scroll event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+buildNav();
+document.addEventListener('DOMContentLoaded',activateSection);
+document.addEventListener('scroll',activateSection);
+// Scroll to section on click
+
+
